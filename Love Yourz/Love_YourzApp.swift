1f3fe0019892bc2d51caef2bb017f275bedd03d6
Sanchitha Dinesh <sanchunila@gmail.com>
2024-04-06 10:47:15 -0700

    Initial Commit

---
+++ Love Yourz/Love_YourzApp.swift
@@ -0,0 +1,17 @@
+//
+//  Love_YourzApp.swift
+//  Love Yourz
+//
+//  Created by Sanchitha Dinesh on 4/6/24.
+//
+
+import SwiftUI
+
+@main
+struct Love_YourzApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
